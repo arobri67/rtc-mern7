8cddac76114d67757b4282cc4c576a233fa7d8c3
Arnaud <arnaud.obri@proton.me>
2024-02-01 16:16:27 +0100

    add cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 require("./config/db");
+const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 const baseRoutes = require("./routes/baseRoutes");
 const authRouter = require("./routes/auth");
 const cageRoutes = require("./routes/cageRoutes");
@@ -8,7 +10,34 @@ const mouseRoutes = require("./routes/mouseRoutes");
 const { seedDataInitDB } = require("./repositories/seedFunctions");
 
 const app = express();
-app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+const limiter = rateLimit({
+  windowMs: 3 * 60 * 1000, // 15 minutes
+  limit: 30, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: false, // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+});
+// Apply the rate limiting middleware to all requests.
+app.use(limiter);
+
+//limit size of the request
+app.use(express.json({ limit: "1mb" }));
+app.use(express.urlencoded({ limit: "1mb", extended: true }));
+
+app.use((req, res, next) => {
+  res.header({
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+    "Access-Control-Allow-Headers": "Content-Type",
+  });
+  next();
+});
+app.disable("x-powered-by");
 
 // Inital DATA Seeding
 seedDataInitDB();
